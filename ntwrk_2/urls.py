5eb005b1a814067147507e6cd7bc8d5685a128fc
bradleykenny <bkenny283@gmail.com>
2018-01-28 19:53:51 +1100

    Cleaned up urls.

--- ntwrk_2/urls.py
+++ ntwrk_2/urls.py
@@ -6,21 +6,20 @@ import account.views
 import stream.views
 
 urlpatterns = [
-	# sub-parts to app
 	# path('account/', include('account.urls')),
 	# path('stream/', include('stream.urls')),
 
-	# THIS IS HOW TO DO THE APP
 	path('login/', account.views.account_login, name='login'),
 	path('register/', account.views.register, name='register'),
 	path('logout/', auth_views.logout, { 'template_name': 'account/logout.html' }, name='logout'),
+
+	path('profile/update/', account.views.update_profile, name='update_profile'),
 
 	path('stream/', stream.views.home, name='home'),
 	path('stream/new_post/', stream.views.new_post, name='new_post'),
 	path('stream/<post_id>/delete_post/', stream.views.delete_post, name='delete_post'),
 	path('stream/<post_id>/view_post/', stream.views.view_post, name='view_post'),
 
-	path('update/', account.views.update_profile, name='update_profile'),
 	# access to admin tools
 	path('admin/', admin.site.urls),
 ]
