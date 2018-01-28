d348f177760ae8b47a69ae57a0eddd18b94a966d
bradleykenny <bkenny283@gmail.com>
2018-01-28 17:57:39 +1100

    Implementing Profile model.

--- account/models.py
+++ account/models.py
@@ -4,15 +4,26 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     location = models.CharField(max_length=30, blank=True)
-#
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+class Profile(models.Model):
+    COLORS = (
+        ('BLUE', "Blue"),
+        ('GREEN', "Green"),
+        ('RED', "Red"),
+        ('PURPLE', "Purple"),
+        ('YELLOW', "Yellow"),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=250, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    color = models.CharField(max_length=6, choices=COLORS, default='BLUE')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
